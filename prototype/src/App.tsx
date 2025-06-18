72a8a59fcc329e8adb405dbeb3007809e72fe706
adimi9 <adirm1609@gmail.com>
2025-06-18 16:23:52 +0800

    added 'Projects Submitted' page

--- prototype/src/App.tsx
+++ prototype/src/App.tsx
@@ -5,20 +5,22 @@ import SubmitUseCasePage from './pages/SubmitUseCasePage'
 import RemoveTediumPage from './pages/RemoveTediumPage'
 import DriveInnovationPage from './pages/DriveInnovationPage'
 import WorkSmarterPage from './pages/WorkSmarterPage'
+import ProjectsSubmittedPage from './pages/ProjectsSubmittedPage'
 
 function App() {
   return (
-    <Router>
-      <div className="min-h-screen bg-black">
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/submit-use-case" element={<SubmitUseCasePage />} />
-          <Route path="/remove-tedium" element={<RemoveTediumPage />} />
-          <Route path="/drive-innovation" element={<DriveInnovationPage />} />
-          <Route path="/work-smarter" element={<WorkSmarterPage />} />
-        </Routes>
-      </div>
-    </Router>
+      <Router>
+        <div className="min-h-screen bg-black">
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/submit-use-case" element={<SubmitUseCasePage />} />
+            <Route path="/projects-submitted" element={<ProjectsSubmittedPage />} />
+            <Route path="/remove-tedium" element={<RemoveTediumPage />} />
+            <Route path="/drive-innovation" element={<DriveInnovationPage />} />
+            <Route path="/work-smarter" element={<WorkSmarterPage />} />
+          </Routes>
+        </div>
+      </Router>
   )
 }
 
